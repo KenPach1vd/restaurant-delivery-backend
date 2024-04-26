c2b0627eafc8fbae845ffe511a685565d1d76471
Vlad Polyakov <s9693118@gmail.com>
2024-04-26 13:25:37 +0300

    Cart functions

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import connectDB from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config.js'
+import cartRouter from "./routes/cartRoute.js"
 
 //app config
 const app = express()
@@ -17,12 +20,14 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res) => {
     res.send("API WORKING!")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
 })
 
